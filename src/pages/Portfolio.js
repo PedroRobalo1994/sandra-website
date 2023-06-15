d0892523b569c55807cd609b458439e6929147c3
pedromfe <pedro-miguel.ferreira-robalo@capgemini.com>
2023-06-15 23:25:20 +0100

    Developed portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -10,9 +10,41 @@ import { Link } from 'react-router-dom';
 // TODO: Continue Portfolio Page
 
 const Portfolio = () => {
-  return <section className='section'>
-    <div className='container mx-auto h-full relative'></div>
-  </section>;
+  return(
+    <section className='section'>
+      <div className='container mx-auto h-full relative'>
+        <div className='flex flex-col lg:flex-row h-full items-center justify-start  gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8'>
+          {/* text */}
+          <div className='flex flex-col lg:items-start'>
+            <h1 className='h1'>Portfolio</h1>
+            <p className='mb-12 max-w-sm'>
+            Loren ipsum dolor sit amet consecutor
+            <br/>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </p>
+            <Link to={'/contact'} className='btn mb-[30px] mx-auto lg:mx-0'>Hire me</Link>
+          </div>
+          {/* image grid */}
+          <div className='grid grid-cols-2 lg:gap-2'>
+            {/* image */}
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all dur500' src={Image1} alt=''/>
+            </div>
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all dur500' src={Image2} alt=''/>
+            </div>
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all dur500' src={Image3} alt=''/>
+            </div>
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all dur500' src={Image4} alt=''/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
