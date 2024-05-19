39ff928ac284b150b8d19b80db01bea79336da69
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2024-05-19 17:52:48 +0100

    Change hover color for socials

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -7,12 +7,12 @@ const Socials = () => {
     <ul className='flex gap-x-5'>
       <li>
         <a href='https://www.instagram.com/sandra_camil0' target='_blank' rel='noreferrer'>
-          <ImInstagram className='text-2xl text-green hover:text-primary transition'/>
+          <ImInstagram className='text-2xl text-green hover:text-pink transition'/>
         </a>
       </li>
       <li>
         <a href='https://www.tiktok.com/@sandra_camil0' target='_blank' rel='noreferrer'>
-          <FaTiktok className='text-2xl text-green hover:text-primary transition'/>
+          <FaTiktok className='text-2xl text-green hover:text-pink transition'/>
         </a>
       </li>
     </ul>
